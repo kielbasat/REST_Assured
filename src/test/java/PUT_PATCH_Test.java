6ac0a75313a407bf35f89d33c7c2ea36c04e2532
kielbasat <t.kielbasa@tlen.pl>
2022-03-14 14:43:09 +0100

    Added JSON object in PUT

--- src/test/java/PUT_PATCH_Test.java
+++ src/test/java/PUT_PATCH_Test.java
@@ -1,5 +1,6 @@
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,33 +9,37 @@ import static io.restassured.RestAssured.given;
 public class PUT_PATCH_Test {
     @Test
     public void updateUserPUT() {
-        String jsonBody = "{\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"Bartek PUT\",\n" +
-                "    \"username\": \"Bartek PUT\",\n" +
-                "    \"email\": \"Bartek@april.put\",\n" +
-                "    \"address\": {\n" +
-                "        \"street\": \"Kulas Light\",\n" +
-                "        \"suite\": \"Apt. 556\",\n" +
-                "        \"city\": \"Gwenborough\",\n" +
-                "        \"zipcode\": \"92998-3874\",\n" +
-                "        \"geo\": {\n" +
-                "            \"lat\": \"-37.3159\",\n" +
-                "            \"lng\": \"81.1496\"\n" +
-                "        }\n" +
-                "    },\n" +
-                "    \"phone\": \"1-770-736-8031 x56442\",\n" +
-                "    \"website\": \"hildegard.org\",\n" +
-                "    \"company\": {\n" +
-                "        \"name\": \"Romaguera-Crona\",\n" +
-                "        \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
-                "        \"bs\": \"harness real-time e-markets\"\n" +
-                "    }\n" +
-                "}";
+
+        JSONObject user = new JSONObject();
+        user.put("name","Bartek PUT");
+        user.put("username","Bartek PUT");
+        user.put("email","Bartek@april.put");
+        user.put("phone","1-770-736-8031 x56442");
+        user.put("website","hildegard.org");
+
+        JSONObject geo = new JSONObject();
+        geo.put("lat","-37.3159");
+        geo.put("lng","81.1496");
+
+        JSONObject address = new JSONObject();
+        address.put("street","Kulas Light");
+        address.put("suite","Apt. 556");
+        address.put("city","Gwenborough");
+        address.put("zipcode","92998-3874");
+        address.put("geo", geo);
+
+        user.put("address",address);
+
+        JSONObject company = new JSONObject();
+        company.put("name", "Romaguera-Crona");
+        company.put("catchPhrase", "Multi-layered client-server neural-net");
+        company.put("bs", "harness real-time e-markets");
+
+        user.put("company",company);
 
         Response response = given()
                 .contentType("application/json")
-                .body(jsonBody)
+                .body(user.toString())
                 .when()
                 .put("https://jsonplaceholder.typicode.com/users/1")
                 .then()
@@ -49,24 +54,24 @@ public class PUT_PATCH_Test {
         Assertions.assertEquals("Bartek@april.put", json.get("email"));
 
     }
-
-    @Test
-    public void userEmailUpdatePatch() {
-
-        String jsonBody = "{\"email\": \"Bartek@april.patch\"}";
-        Response response = given()
-                .contentType("application/json")
-                .body(jsonBody)
-                .when()
-                .patch("https://jsonplaceholder.typicode.com/users/1")
-                .then()
-                .statusCode(200)
-                .extract()
-                .response();
-
-        JsonPath json = response.jsonPath();
-
-        Assertions.assertEquals("Bartek@april.patch", json.get("email"));
-    }
+//
+//    @Test
+//    public void userEmailUpdatePatch() {
+//
+//        String jsonBody = "{\"email\": \"Bartek@april.patch\"}";
+//        Response response = given()
+//                .contentType("application/json")
+//                .body(jsonBody)
+//                .when()
+//                .patch("https://jsonplaceholder.typicode.com/users/1")
+//                .then()
+//                .statusCode(200)
+//                .extract()
+//                .response();
+//
+//        JsonPath json = response.jsonPath();
+//
+//        Assertions.assertEquals("Bartek@april.patch", json.get("email"));
+//    }
 }
 
